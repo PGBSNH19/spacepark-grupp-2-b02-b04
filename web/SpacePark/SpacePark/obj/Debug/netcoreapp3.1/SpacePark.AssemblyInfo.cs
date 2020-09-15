ee111658b65d802a303e59e731ff8f1bd9e50c09
singelmamma <55433931+singelmamma@users.noreply.github.com>
2020-09-15 14:06:15 +0200

    dockerfile

--- web/SpacePark/SpacePark/obj/Debug/netcoreapp3.1/SpacePark.AssemblyInfo.cs
+++ web/SpacePark/SpacePark/obj/Debug/netcoreapp3.1/SpacePark.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("1b493fe2-ffeb-4344-9613-19e742fcdd07")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpacePark")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]

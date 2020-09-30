c06b9a4a970d75acb578fefa0ef2aa2e22683060
robodostopif <boomeraron@gmail.com>
2020-09-30 10:16:06 +0200

    Added keyvault to application

--- web/SpacePark/SpacePark/Db_Context/SpaceParkContext.cs
+++ web/SpacePark/SpacePark/Db_Context/SpaceParkContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using SpacePark.Services;
 using System.IO;
 
 namespace SpacePark.Models
@@ -7,25 +8,27 @@ namespace SpacePark.Models
     public class SpaceParkContext : DbContext
     {
         public SpaceParkContext() { }
+        AzureKeyVaultService _aKVService = new AzureKeyVaultService();
         public DbSet<Parkinglot> Parkinglot { get; set; }
         public DbSet<Person> People { get; set; }
         public DbSet<Spaceship> Spaceships { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            try
+            var azureDbCon = _aKVService.GetKeyVaultSecret("https://spacepark-kv-dev-01.vault.azure.net/secrets/ConnectionStrings--spacepark-sqldb-dev-01/f54483ed80744f0bad0bdfb31203d786");
+            var builder = new ConfigurationBuilder();
+            if (string.IsNullOrEmpty(azureDbCon))
             {
-                var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+                builder.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
                 var config = builder.Build();
                 var defaultConnectionString = config.GetConnectionString("DefaultConnection");
                 optionsBuilder.UseSqlServer(defaultConnectionString);
+
             }
-            catch
+            else
             {
-                var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("AppsettingsDeploy.json");
-                var config = builder.Build();
-                var defaultConnectionString = config.GetConnectionString("SpaceparkDatabase");
-                optionsBuilder.UseSqlServer(defaultConnectionString);
+                builder.Build();
+                optionsBuilder.UseSqlServer(azureDbCon);
             }
         }
     }
